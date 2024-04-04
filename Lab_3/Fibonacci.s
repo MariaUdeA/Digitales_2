1f53dd81cac47d1255e92705b322ea7a8f5c4840
juliansanchez6 <julian.sanchez6@udea.edu.co>
2024-04-04 14:38:01 -0500

    actualización juanita

--- Lab_3/Fibonacci.s
+++ Lab_3/Fibonacci.s
@@ -1,40 +1,51 @@
 .global _start
-	.equ MAXN, 	30
+	.equ MAXN, 30	
 	.text
 _start:
 	/* Inicio de programa:
 	 * Inicialización de registros y lectura de valores requeridos la memoria
 	 */
 	 
-	 
-	 
-	 
-	 
 	
 	/* Cuerpo del programa:
 	 *Codigo principal para generar los valores solicitados de la serie Finonnacci
 	 */
+	 	 
 	 LDR R0, =vector
-	 MOV R1, #N
+	 LDR R2, =SortedValues 
+	 LDR R1, =N
+	 LDR R1, [R1]
+	 LDR R9, =a1
+	 LDR R9, [R9]
 	 
-	 CMP R1, #N
-	 BLE _stop
 	 
-	 SUB
+	 //Interrupción cuando el N es menor a 5
+	 CMP R1, #5
+	 STR R9, [R2]
+	 BLO _stop
 	 
 	 
+	 //Interrupción cuando N es mayor a 30
+	 CMP R1, #30
+	 STR R9, [R2]
+	 BHI _stop
+	 	 
 	 BL Fibo
+	
+	//Corregir que esto no genera el 0
+	
 	
 Fibo:
 	MOV R2, #1
-	STR R2, [R0, #4]
+	STR R2, [R0], #4
+	STR R2, [R0], #4
 	SUB R1, R1, #2
 
 Loop:
 	LDR R2, [R0, #-8]
 	LDR R3, [R0, #-4]
 	ADD R3, R2, R3
-	STR R3, [R0, #4]
+	STR R3, [R0], #4
 	
 	SUBS R1, R1, #1
 	BNE Loop
@@ -55,11 +66,12 @@ _stop:
  .data
  
  /*
-  *Lista de variables
+  *Lista de variables en la memoria
   */
-
+a1: 			.DC.L 0xA5A5A5A5
 N:				.DC.L 5
 POS:			.DC.L 2,9
 ORDER:			.DC.L 1
-vector:			.DS.L N
+vector:			.DS.L 5
 SortedValues:	.DC.L MAXN
+
