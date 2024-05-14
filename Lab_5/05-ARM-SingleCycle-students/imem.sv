35345aed35b75d140d3b7b23010c3d9fe6c8a238
juliansanchez6 <julian.sanchez6@udea.edu.co>
2024-05-14 09:38:48 -0500

    pruebas

--- Lab_5/05-ARM-SingleCycle-students/imem.sv
+++ Lab_5/05-ARM-SingleCycle-students/imem.sv
@@ -13,7 +13,5 @@ module imem(input logic [31:0] a, output logic [31:0] rd);
 
 		// Uncomment the following line only if you want to load the code made by your group
 		 $readmemh("C:/Users/julia/Desktop/Universidad/Electronica Digital 2/Laboratorio/Lab_5/05-ARM-SingleCycle-students/imem_made_by_students.dat",RAM);
-//		 $readmemh("F:/Digitales 2/2024-1/Laboratorio/Laboratorio 5/Proyecto5_final/imems/imem_made_by_students.dat",RAM);
-
 	assign rd = RAM[a[31:2]]; // word aligned
 endmodule
