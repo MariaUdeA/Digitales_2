65f39d5800901929609fc5913334c6410795fd73
unknown <maria.arbelaezs@udea.edu.co>
2024-05-14 09:32:58 -0500

    mal display

--- Lab_5/05-ARM-SingleCycle-students/imem.sv
+++ Lab_5/05-ARM-SingleCycle-students/imem.sv
@@ -12,8 +12,13 @@ module imem(input logic [31:0] a, output logic [31:0] rd);
 //		$readmemh("imem_to_test_peripherals.dat",RAM);
 
 		// Uncomment the following line only if you want to load the code made by your group
-		 $readmemh("C:/Users/julia/Desktop/Universidad/Electronica Digital 2/Laboratorio/Lab_5/05-ARM-SingleCycle-students/imem_made_by_students.dat",RAM);
-//		 $readmemh("F:/Digitales 2/2024-1/Laboratorio/Laboratorio 5/Proyecto5_final/imems/imem_made_by_students.dat",RAM);
+		//Julian:
+		//$readmemh("C:/Users/julia/Desktop/Universidad/Electronica Digital 2/Laboratorio/Lab_5/05-ARM-SingleCycle-students/imem_made_by_students.dat",RAM);
+		//Maria: 
+		$readmemh("C:/Users/Compu de Maria/Desktop/UdeA/semestre6/digi2/Digitales_2/Lab_5/05-ARM-SingleCycle-students/imem_made_by_students.dat",RAM);
+
+		
+		//$readmemh("F:/Digitales 2/2024-1/Laboratorio/Laboratorio 5/Proyecto5_final/imems/imem_made_by_students.dat",RAM);
 
 	assign rd = RAM[a[31:2]]; // word aligned
 endmodule
